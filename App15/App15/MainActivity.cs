ca46afa1acead2c94f95266f7fc65bd3d45e11cd
NeetjeBroers <nathan.broers2000@gmail.com>
2019-09-16 14:01:43 +0200

    Divide Beginning

--- App15/App15/MainActivity.cs
+++ App15/App15/MainActivity.cs
@@ -6,6 +6,11 @@ using Android.Widget;
 using Android.Views;
 using Xamarin.Essentials;
 using System;
+using Newtonsoft.Json;
+using System.Runtime.Serialization.Json;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
 
 namespace App15
 {
@@ -23,10 +28,23 @@ namespace App15
             var textview = FindViewById<TextView>(Resource.Id.Textview);
             var ButtonOne = FindViewById<Button>(Resource.Id.button1);
 
+
             ButtonOne.Click += (sender, e) =>
             {
+                string number1 = Convert.ToString(edittext.Text);
+                int number2 = Convert.ToInt32(number1);
+                string responseString = GetStudentGroups(number2);
 
-                textview.Text = edittext.Text;
+                var groups = JsonConvert.DeserializeObject<List<Group>>(responseString);
+                foreach (var group in groups)
+                {
+                    Console.WriteLine(group.GroupNumber);
+                    foreach (var student in group.Students)
+                    {
+                        Console.WriteLine("  -" + student.Name);
+                    }
+                }
+                //textview.Text = edittext.Text;
 
             };
 
@@ -39,7 +57,7 @@ namespace App15
                 Accelerometer.Stop();
             }
 
-            
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
@@ -47,6 +65,41 @@ namespace App15
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+        private static string GetStudentGroups(int size)
+        {
+            var request = WebRequest.Create("https://classdivider.azurewebsites.net/api/GetStudentGroups?code=8QhjS0G4vDQIM2IatwNjZjWxfLOXWei0XcwyO/2htLhC20qQD15nDg==");
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            var groupSize = size;
+            var data = "{\"GroupSize\":" + groupSize + "}";
+            using (var writer = new StreamWriter(request.GetRequestStream()))
+            {
+                writer.WriteLine(data);
+            }
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            return responseString;
+        }
+
+        public class Student
+        {
+            public string StudentNumber { get; set; }
+            public string Name { get; set; }
+            public string SurName { get; set; }
+            public string StudentClass { get; set; }
+        }
+
+        public class Group
+        {
+            public int GroupNumber { get; set; }
+            public List<Student> Students { get; set; }
+        }
+
+
+
+
 
 
 
