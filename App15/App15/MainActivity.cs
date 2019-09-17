c93853b84ad77ca3cb0e1f36cc5a03e735a667a0
NeetjeBroers <nathan.broers2000@gmail.com>
2019-09-17 09:09:47 +0200

    Update MainActivity.cs

--- App15/App15/MainActivity.cs
+++ App15/App15/MainActivity.cs
@@ -44,7 +44,7 @@ namespace App15
                         Console.WriteLine("  -" + student.Name);
                     }
                 }
-                //textview.Text = edittext.Text;
+                textview.Text = responseString;
 
             };
 
